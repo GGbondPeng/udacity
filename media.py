6af825616fe0c5830af4e6e7ce6d3ce6d5dad0db
leitianpeng <leitianpeng@uboxol.com>
2017-09-12 21:17:56 +0800

    third

--- media.py
+++ media.py
@@ -2,10 +2,12 @@ import webbrowser
 class Movie():
     '''class for create movie obj'''
     def __init__(self,title,movie_url,poster_image_url):
+        '''init movie with variable title,movie_url,poster_image_url'''
         self.title = title
         self.trailer_youtube_url = movie_url
         self.poster_image_url = poster_image_url
     def show_movie(self):
+        '''open movie_url in brower'''
         webbrowser.open(self.movie_url)
 
         
